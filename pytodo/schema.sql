b4ea531cb3d5c13264e665454f37808d73b7e4cc
Laura Sanchez <lausanch0512@gmail.com>
2022-07-19 18:50:44 -0500

    Changed table names

--- pytodo/schema.sql
+++ pytodo/schema.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
 
-CREATE TABLE user (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE task (
+CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     description TEXT NOT NULL,
     done BOOLEAN NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
