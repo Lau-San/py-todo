000df4b178e958732530009a50272873670b9194
Laura Sanchez <lausanch0512@gmail.com>
2022-07-20 17:09:56 -0500

    Migrated all tasks database actions to PostgreSQL

--- pytodo/tasks.py
+++ pytodo/tasks.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, flash, g, request, redirect, url_for
+from psycopg2.extras import DictCursor
 from werkzeug.exceptions import abort
 
 from .auth import login_required
@@ -10,9 +11,25 @@ bp = Blueprint('tasks', __name__, url_prefix='/tasks')
 @bp.route('/', methods=('GET', 'POST'))
 @login_required
 def index():
-    tasks = get_db().execute('SELECT * FROM task'
-                             ' WHERE user_id = ?',
-                             (g.user['id'],)).fetchall()
+    db = get_db()
+    with db.cursor(cursor_factory=DictCursor) as cur:
+        cur.execute(
+            '''
+            SELECT
+                id,
+                description,
+                done
+            FROM tasks
+            WHERE user_id = %s
+            ''',
+            (g.user['id'],)
+        )
+        tasks = cur.fetchall()
+
+    # tasks = get_db().execute('SELECT * FROM task'
+    #                          ' WHERE user_id = ?',
+    #                          (g.user['id'],)).fetchall()
+
     todo = list(filter(lambda x: x['done'] == False, tasks))
     done = list(filter(lambda x: x['done'] == True, tasks))
     return render_template('tasks/tasks.html', tasks=tasks, todo=todo, done=done)
@@ -21,13 +38,13 @@ def index():
 @bp.route('/create', methods=('POST',))
 @login_required
 def create():
-    if request.method == 'POST':
-        task_desc = request.form['description']
+    task_desc = request.form['description']
 
-        if task_desc != '':
-            db = get_db()
-            db.execute(
-                'INSERT INTO task (user_id, description, done) VALUES (?, ?, ?)',
+    if task_desc != '':
+        db = get_db()
+        with db.cursor() as cur:
+            cur.execute(
+                'INSERT INTO tasks (user_id, description, done) VALUES (%s, %s, %s)',
                 (g.user['id'], task_desc, False))
             db.commit()
 
@@ -35,16 +52,19 @@ def create():
 
 
 def get_task(id, check_user=True):
-    task = get_db().execute(
-        'SELECT * FROM task WHERE id = ?',
-        (id,)
-    ).fetchone()
+    with get_db().cursor(cursor_factory=DictCursor) as cur:
+        cur.execute(
+            'SELECT * FROM tasks WHERE id = %s',
+            (id,)
+        )
 
-    if task is None:
-        abort(404, f"Task id {id} doesn't exist.")
+        task = cur.fetchone()
 
-    if check_user and task['user_id'] != g.user['id']:
-        abort(403)
+        if task is None:
+            abort(404, f"Task id {id} doesn't exist.")
+
+        if check_user and task['user_id'] != g.user['id']:
+            abort(403)
 
     return task
 
@@ -53,10 +73,21 @@ def get_task(id, check_user=True):
 @login_required
 def done(id):
     get_task(id)
+
     db = get_db()
-    db.execute('UPDATE task SET done = True'
-               ' WHERE id = ?', (id,))
-    db.commit()
+    with db.cursor() as cur:
+        cur.execute(
+            '''
+            UPDATE tasks
+            SET done = True
+            WHERE id = %s
+            ''',
+            (id,)
+        )
+        db.commit()
+    # db.execute('UPDATE task SET done = True'
+    #            ' WHERE id = ?', (id,))
+    # db.commit()
     return redirect(url_for('tasks.index'))
 
 
@@ -64,10 +95,21 @@ def done(id):
 @login_required
 def undone(id):
     get_task(id)
+
     db = get_db()
-    db.execute('UPDATE task SET done = False'
-               ' WHERE id = ?', (id,))
-    db.commit()
+    with db.cursor() as cur:
+        cur.execute(
+            '''
+            UPDATE tasks
+            SET done = False
+            WHERE id = %s
+            ''',
+            (id,)
+        )
+        db.commit()
+    # db.execute('UPDATE task SET done = False'
+    #            ' WHERE id = ?', (id,))
+    # db.commit()
     return redirect(url_for('tasks.index'))
 
 
@@ -75,7 +117,11 @@ def undone(id):
 @login_required
 def delete(id):
     get_task(id)
+
     db = get_db()
-    db.execute('DELETE FROM task WHERE id = ?', (id,))
-    db.commit()
+    with db.cursor() as cur:
+        cur.execute('DELETE FROM tasks WHERE id = %s', (id,))
+        db.commit()
+    # db.execute('DELETE FROM task WHERE id = ?', (id,))
+    # db.commit()
     return redirect(url_for('tasks.index'))
